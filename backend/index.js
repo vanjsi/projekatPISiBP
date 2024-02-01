4d6ad4c89450e92dca7ca855fa09f70c54e49503
vanjsi <radovanovic.vanja0406@gmail.com>
2024-02-01 20:16:03 +0100

    Nova izmena - backedn

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const app=express()
+const dotenv=require("dotenv")
+const authRoute=require("./routes/auth")
+const userRoute=require("./routes/users")
+const postRoute=require("./routes/posts")
+const commentRoute=require("./routes/comments")
+
+
+//database
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully!")
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+//middlewares
+dotenv.config()
+app.use(express.json())
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/comments",commentRoute)
+
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running on port "+process.env.PORT)
+})
